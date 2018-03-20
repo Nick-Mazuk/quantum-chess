9cd7cbf1fd417b22ee447c77457d1955b62ea119
Aaron Stanek <nope>
2018-03-19 21:58:10 -0700

    included skeleton for backend

---
+++ backend/code_reference.py
@@ -0,0 +1,19 @@
+# stolen from
+# http://websockets.readthedocs.io/en/stable/intro.html
+# this will form the basis for the backend code
+
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print("< {}".format(name))
+
+    greeting = "Hello {}!".format(name)
+    await websocket.send(greeting)
+    print("> {}".format(greeting))
+
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
